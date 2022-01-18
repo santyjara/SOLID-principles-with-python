0c336d5003ce02e5f9cd101df73c449afeefc525
Santiago Jaramillo <san_jara@outlook.com>
2022-01-17 23:40:24 -0500

    Add model interface - Add an attribute -> model_path - Add abstract methods -> train, predict, eval - Add a context to use those strategies

---
+++ 4. Interface Segregation Principle - ISP/base.py
@@ -0,0 +1,84 @@
+from abc import ABC, abstractmethod
+from typing import Dict, List, Union
+
+
+class ModelStrategy(ABC):
+    """The strategy interface declares operations common to all
+    supported versions of some algorithm. The context uses this
+    interface to call the algorithm defined by the concrete
+    strategies.
+    Attributes
+    ----------
+    model_path : str
+        Path where a pre-trained model is stored
+    """
+
+    def __init__(self, model_path: str):
+        self.model_path = model_path
+
+    @abstractmethod
+    def train(
+        self,
+        x_train: List[str],
+        y_train: List[str],
+        x_test: List[str],
+        y_test: List[str],
+        params: Dict[str, Union[str, int]],
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def predict(self, samples: Union[str, list[str]]) -> List[int]:
+        pass
+
+    @abstractmethod
+    def eval(self, x_test: List[str], y_test: List[str]) -> Dict[str, float]:
+        pass
+
+
+class Model:
+    """The context maintains a reference to one of the strategy
+    objects. The context doesn't know the concrete class of a
+    strategy. It should work with all strategies via the
+    strategy interface.
+    Attributes
+    ----------
+    strategy : ModelStrategy
+        Model strategy to be used
+    """
+    def __init__(self, strategy: ModelStrategy) -> None:
+        self._strategy = strategy
+
+    @property
+    def strategy(self) -> ModelStrategy:
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, strategy: ModelStrategy) -> None:
+        self._strategy = strategy
+
+    def train(
+        self,
+        x_train: List[str],
+        y_train: List[str],
+        x_test: List[str],
+        y_test: List[str],
+        params: Dict[str, Union[str, int]],
+    ) -> None:
+        print("Training ....", end=" ")
+        self._strategy.train(x_train, y_train, x_test, y_test, params)
+        print("Finished !!")
+
+    def __call__(self, samples: Union[str, list[str]]) -> List[int]:
+        results = self._strategy.predict(samples)
+        print(f"Predictions: \n \t {results}")
+        return results
+
+    def eval(self, x_test: List[str], y_test: List[str]) -> Dict[str, float]:
+        print("Evaluating model ....", end=" ")
+        metrics = self._strategy.eval(x_test, y_test)
+        print("ok")
+        print("\n Model performance metrics:", end="\n * ")
+        for metric, value in metrics.items():
+            print(f"{metric}: {value}", end="\n * ")
+        return metrics
